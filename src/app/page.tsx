5a7a0b27c98ad17acaba432aee9f74c54fef3e9c
brian <brianmstricker@gmail.com>
2024-07-10 01:09:07 -0400

    wip: why choose us cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import heroImage from "@/assets/hero-img3.jpg";
 import HomeSearchbar from "@/components/home/HomeSearchbar";
 import BrowseByType from "@/components/home/BrowseByType";
+import ChooseUsCard from "@/components/home/ChooseUsCard";
 
 export default function Home() {
  return (
@@ -19,8 +20,8 @@ export default function Home() {
       loading="eager"
      />
      <div className="text-white">
-      <p className="text-center text-sm sm:text-base">Search from a rich collection of rides.</p>
-      <h1 className="text-center text-2xl sm:text-6xl tracking-wider font-semibold leading-relaxed">Find the perfect vehicle</h1>
+      <p className="text-center text-sm sm:text-base">Search from a rich collection of vehicles.</p>
+      <h1 className="text-center text-[7vw] sm:text-5xl md:text-6xl tracking-wider font-bold leading-relaxed">Find the perfect ride</h1>
       <div className="max-w-[757px]">
        {/* <HomeSearchbar /> */}
        <div className="mt-2 text-center">or</div>
@@ -29,11 +30,20 @@ export default function Home() {
      </div>
     </div>
    </section>
-   <section className="contain px-4 mt-20 grid grid-cols-2 gap-10">
-    <div className="bg-blue-200 dark:bg-blue-400 h-64 rounded-md p-4 flex items-end">
-     <h2 className="">Looking to sell your vehicle?</h2>
-    </div>
-    <div className="bg-red-300 h-64 rounded-md p-4">2</div>
+   <section className="contain px-4 mt-20">
+    <h2 className="text-center text-[7vw] sm:text-4xl font-bold">Why Choose Us?</h2>
+    {/* <div className="grid grid-cols-4 gap-10 mt-4 h-40">
+     <ChooseUsCard />
+     <ChooseUsCard />
+     <ChooseUsCard />
+     <ChooseUsCard />
+    </div> */}
+    {/* <div className="grid grid-cols-2 gap-10">
+     <div className="bg-blue-200 dark:bg-blue-400 h-64 rounded-md p-4 flex items-end">
+      <h2 className="">Looking to sell your vehicle?</h2>
+     </div>
+     <div className="bg-red-200 dark:bg-red-400 h-64 rounded-md p-4">2</div>
+    </div> */}
    </section>
   </main>
  );
---
+++ src/components/home/ChooseUsCard.tsx
@@ -0,0 +1,4 @@
+const ChooseUsCard = ({ title, description, image }: { title: string; description: string; image: string }) => {
+ return <div className="border">ChooseUsCard</div>;
+};
+export default ChooseUsCard;

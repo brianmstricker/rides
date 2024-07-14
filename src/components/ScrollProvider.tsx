89a52a6898cf1e38ae8692fb4bb26ea6da203beb
brian <brianmstricker@gmail.com>
2024-07-14 15:47:14 -0400

    fix: scroll provider works on refresh, resets on navigation

--- src/components/ScrollProvider.tsx
+++ src/components/ScrollProvider.tsx
@@ -1,13 +1,18 @@
 "use client";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export function ScrollProvider({ children }: Readonly<{ children: React.ReactNode }>) {
  const [mounted, setMounted] = useState(false);
  const [isRestoring, setIsRestoring] = useState(true);
+ const path = usePathname();
  useEffect(() => setMounted(true), []);
  useEffect(() => {
   const saveScrollPosition = () => {
    sessionStorage.setItem("scrollPosition", window.scrollY.toString());
+  };
+  const resetScrollPosition = () => {
+   sessionStorage.removeItem("scrollPosition");
   };
   window.addEventListener("beforeunload", saveScrollPosition);
   window.addEventListener("popstate", saveScrollPosition);
@@ -19,12 +24,16 @@ export function ScrollProvider({ children }: Readonly<{ children: React.ReactNod
  useEffect(() => {
   if (mounted) {
    const scrollPosition = sessionStorage.getItem("scrollPosition");
-   if (scrollPosition) {
+   const navigationEntries = performance.getEntriesByType("navigation") as PerformanceNavigationTiming[];
+   if (scrollPosition && navigationEntries[0]?.type === "reload") {
     window.scrollTo(0, parseFloat(scrollPosition));
+   } else {
+    console.log("here");
+    window.scrollTo(0, 0);
    }
    setIsRestoring(false);
   }
- }, [mounted]);
+ }, [mounted, path]);
  if (!mounted) return null;
  return <div style={{ visibility: isRestoring ? "hidden" : "visible" }}>{children}</div>;
 }

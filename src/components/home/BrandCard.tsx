13dc020dda520022de02d08715b2c5559f9bcd15
brian <brianmstricker@gmail.com>
2024-07-19 15:59:56 -0400

    fix: old props on next image comp

--- src/components/home/BrandCard.tsx
+++ src/components/home/BrandCard.tsx
@@ -8,7 +8,7 @@ const BrandCard = () => {
    className="border px-4 py-2 rounded-md shadow shadow-foreground/10 dark:shadow-foreground/5 cursor-pointer transition-all duration-300 hover:shadow-lg hover:scale-105 focus-visible:shadow-lg focus-visible:scale-105 outline-none focus-visible:ring ring-foreground/20 focus-visible:border-transparent"
   >
    <div className="relative w-20 h-20 mx-auto">
-    <Image src={audiLogo} alt="audi" layout="fill" objectFit="contain" />
+    <Image src={audiLogo} alt="audi" fill className="object-contain" />
    </div>
    <h3 className="text-center font-bold text-2xl sm:text-lg md:text-2xl">Audi</h3>
   </div>

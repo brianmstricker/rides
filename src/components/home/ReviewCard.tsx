075f459a42e4ebc65cb0d9d42bb14d92cdd7079d
brian <brianmstricker@gmail.com>
2024-07-26 00:10:58 -0400

    feat: review section responsiveness

--- src/components/home/ReviewCard.tsx
+++ src/components/home/ReviewCard.tsx
@@ -1,19 +1,43 @@
 import { cn } from "@/lib/utils";
 import { Star } from "lucide-react";
 
-const ReviewCard = ({ className }: { className?: string }) => {
+const ReviewCard = ({ i, className }: { i: number; className?: string }) => {
+ let classes = "";
+ let description = "";
+ switch (i) {
+  case 0:
+   classes = "md:translate-y-6 order-2 md:order-none";
+   description =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec id felis eget libero ultricies aliquet. Donec id felis eget libero ultricies aliquet.";
+   break;
+  case 1:
+   classes =
+    "relative before:absolute before:border-yellow-600 before:border before:bg-background before:inset-0 before:z-[-1] before:-bottom-2 bg-background before:-left-2 before:mt-2 before:mr-2 sm:ml-2 order-1 md:order-none before:opacity-0 sm:before:opacity-100";
+   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+   break;
+  case 2:
+   classes = "md:translate-y-6 order-3 md:order-none";
+   description = "Sed ut purus eget nunc. Donec id felis eget libero ultricies aliquet. Donec id felis eget libero ultricies aliquet.";
+   break;
+  case 3:
+   classes =
+    "flex flex-col md:hidden lg:flex relative before:absolute before:border-yellow-600 before:border before:bg-background before:inset-0 before:z-[-1] before:-bottom-2 bg-background before:-left-2 before:mt-2 before:mr-2 sm:ml-2 order-4 md:order-none before:opacity-0 sm:before:opacity-100";
+   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+   break;
+  case 4:
+   classes = "flex flex-col sm:hidden xl:flex md:translate-y-6";
+   description = "Sed ut purus eget nunc. Donec id felis eget libero ultricies aliquet. Donec id felis eget libero ultricies aliquet.";
+   break;
+ }
  return (
-  <div className={cn("border px-4 py-3", className)}>
+  <div className={cn("border px-3 lg:px-4 py-3 flex flex-col", classes, className)}>
    <div className="flex gap-3">
     {Array.from({ length: 5 }, (_, i) => (
      <Star size={26} fill="#eab308" className="text-yellow-500" key={i} />
     ))}
    </div>
-   <p className="my-4">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, repellat quo corrupti eos veniam maiores esse optio mollitia nostrum rem
-    numquam, voluptatibus ad est facilis quia voluptates eaque doloremque corporis consectetur dignissimos facere nulla ducimus.
-   </p>
-   <div className="flex items-center">
+   <p className="my-4">{description}</p>
+   <div className="flex items-center mt-auto">
     <div className="w-10 h-10 bg-red-600 rounded-full" />
     <div className="ml-4">
      <h4 className="font-semibold">John Doe</h4>
--- src/components/home/ReviewSection.tsx
+++ src/components/home/ReviewSection.tsx
@@ -1,16 +1,45 @@
+"use client";
+import { cn } from "@/lib/utils";
 import TitleH2 from "../global/TitleH2";
 import ReviewCard from "./ReviewCard";
+import { useState } from "react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 const ReviewSection = () => {
+ const [cardToRender, setCardToRender] = useState(0);
+ function nextCard() {
+  setCardToRender((prev) => (prev === 4 ? 0 : prev + 1));
+ }
+ function prevCard() {
+  setCardToRender((prev) => (prev === 0 ? 4 : prev - 1));
+ }
  return (
-  <section className="contain mt-24 pb-10">
+  <section className="contain mt-24 pb-6">
    <TitleH2 title="Customer Reviews" />
-   <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-    <ReviewCard className="translate-y-10" />
-    <ReviewCard className="relative before:absolute before:border-yellow-600 before:border before:bg-background before:inset-0 before:z-[-1] before:-bottom-2 bg-background before:-left-2 before:mt-2 before:mr-2" />
-    <ReviewCard className="translate-y-10" />
-    <ReviewCard className="block md:hidden lg:block relative before:absolute before:border-yellow-600 before:border before:bg-background before:inset-0 before:z-[-1] before:-bottom-2 bg-background before:-left-2 before:mt-2 before:mr-2" />
-    <ReviewCard className="block sm:hidden xl:block translate-y-10" />
+   <div className="grid [grid-template-rows:1fr] [grid-template-columns:1fr] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 relative">
+    <button
+     onClick={prevCard}
+     className="absolute -bottom-2 -left-3 bg-background p-1 rounded-full border z-[1] sm:hidden opacity-80 focus-visible:opacity-100"
+    >
+     <ArrowLeft />
+    </button>
+    {Array.from({ length: 5 }, (_, i) => (
+     <ReviewCard
+      key={i}
+      i={i}
+      className={cn(
+       cardToRender === i
+        ? "opacity-100 [grid-row:1] [grid-column:1] sm:[grid-row:auto] sm:[grid-column:auto]"
+        : "opacity-0 sm:opacity-100 [grid-row:1] [grid-column:1] sm:[grid-row:auto] sm:[grid-column:auto]"
+      )}
+     />
+    ))}
+    <button
+     onClick={nextCard}
+     className="absolute -bottom-2 -right-3 bg-background p-1 rounded-full border z-[1] sm:hidden opacity-80 focus-visible:opacity-100"
+    >
+     <ArrowRight />
+    </button>
    </div>
   </section>
  );
